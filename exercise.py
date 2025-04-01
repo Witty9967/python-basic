c59b49c2b53b918e5459fcd5776a8338ecab0264
机智 <9879421+jizhigitee@user.noreply.gitee.com>
2025-04-01 18:32:55 +0800

    add exercise.py file

---
+++ exercise.py
@@ -0,0 +1,11 @@
+# 邮件机器人
+username = input('Please input your name: ')
+productname = input('Please input your product name: ')
+# print('Dear ',username, ':')
+# print('Your purchased product :',productname, 'is delivered!')
+# print('Thanks for your choosing!')
+print(f'''
+Dear {username}:
+Your purchased product: {productname} is delivered! 
+Thanks for you choosing!
+''')
